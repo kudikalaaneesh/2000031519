71549fa3f4339431db5ea5581532acdc94cea9b8
2000031519_Aneesh <2000031519@kluniversity.in>
2023-04-29 21:44:54 +0530

    all trains page with backend connection is done

--- Task2/trainsschedule/src/App.js
+++ Task2/trainsschedule/src/App.js
@@ -2,14 +2,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AllTrains from './Componets/AllTrains';
 import SpecificTrain from './Componets/SpecificTrain';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
       <>
         <Router>
+          <Navbar/>
           <Routes>
-            <Route path="alltrains" element={<AllTrains/>} />
-            <Route path="train/:id" element={<SpecificTrain/>} />
+            <Route path="/" element={<AllTrains/>} />
+            <Route path="/train/:id" element={<SpecificTrain/>} />
           </Routes>
       </Router>
         
--- Task2/trainsschedule/src/Componets/AllTrains.js
+++ Task2/trainsschedule/src/Componets/AllTrains.js
@@ -1,8 +1,50 @@
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import axios from "axios";
+import './alltrains.css'
 
 function AllTrains() {
+
+    const [allInfo, setAllInfo] = React.useState([])
+
+    React.useEffect(()=>{
+        axios({
+            url: "http://localhost:3001/trains",
+            method: "GET"
+        })
+        .then((res) => { 
+            setAllInfo(res.data)
+        })
+        .catch((err) => { 
+            console.log(err)
+        });
+    }, []) // empty array to run this effect only once when component mounts
+
     return (
         <>
+         <div className='alltrains'>
+            {
+               
+                allInfo && allInfo.map((data) =>{
+                    return (
+                            <Card style={{ width: '18rem', marginTop:"20px" }}>
+                                <Card.Img variant="top" src={data.img} />
+                                <Card.Body>
+                                    <Card.Title>{data.name}</Card.Title>
+                                    <Card.Text style={{textAlign:"justify"}}>
+                                    {data.info}
+                                    </Card.Text>
+                                    <Button variant="primary">View More</Button>
+                                </Card.Body>
+                            </Card>
+                       
+                    )
+                })
+                
+            }  
+            </div>   
         </>
     )
 }
